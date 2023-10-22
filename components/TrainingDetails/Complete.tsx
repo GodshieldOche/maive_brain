a2c83b327b672708e6bf91e2bcf091fb8bd0e903
GodshieldOche <ggodshield@gmail.com>
2023-10-22 06:38:46 +0100

    [chore]: some updates 2

--- components/TrainingDetails/Complete.tsx
+++ components/TrainingDetails/Complete.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import React, { useEffect } from "react";
 import SuccessGif from "../../public/success.gif";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 const Complete: React.FC<{ answers: string[] }> = ({ answers }) => {
   const router = useRouter();
+  const { id } = useParams();
+
   useEffect(() => {
     if (!answers.length) {
       return router.push("?step=0");
@@ -14,8 +16,8 @@ const Complete: React.FC<{ answers: string[] }> = ({ answers }) => {
     <div className="w-[90vw] lg:w-[41vw] rounded-[15px] flex flex-col justify-center items-center py-6 pb-12 px-9 bg-[#22685D] ">
       <Image src={SuccessGif} alt="Success gif" />
       <div className="flex flex-col gap-y-6 w-full items-center justify-center">
-        <h1 className="text-xl lg:text-[32px] text-center font-semibold text-white leading-[50px] lg:leading-[70px] ">
-          Dyslexia Pre-Screener Completed
+        <h1 className="text-xl lg:text-[32px] capitalize text-center font-semibold text-white leading-[50px] lg:leading-[70px] ">
+          {id} Pre-Screener Completed
         </h1>
         <h1 className="text-base lg:text-xl text-white text-center leading-[50px] ">
           Congratulations!! you have completed this assessment, we will review
--- components/TrainingDetails/GetStarted.tsx
+++ components/TrainingDetails/GetStarted.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Button from "../Common/Button";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 const GetStarted: React.FC = () => {
   const router = useRouter();
+  const { id } = useParams();
   return (
     <div className="w-[90vw] lg:min-h-[45vh] lg:w-[45vw] rounded-[15px] gap-y-12 flex flex-col items-center py-6 px-9 bg-[#22685D] ">
       <div className="flex flex-col gap-y-6 w-full items-center justify-center">
-        <h1 className=" text-xl lg:text-[32px] font-semibold text-white leading-[70px] ">
-          Dyslexia Pre-Screener
+        <h1 className=" text-xl lg:text-[32px] capitalize font-semibold text-white leading-[70px] ">
+          {id} Pre-Screener
         </h1>
         <h1 className=" text-base lg:text-xl text-white text-center leading-[40px] lg:leading-[50px] ">
           Maive Brain is a one-on-one brain training center that uses over
